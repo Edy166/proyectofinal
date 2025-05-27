95b531061a4476d1103dc952df7abf3c34a3b33f
etiguilam <etiguilam1@miumg.edu.gt>
2025-05-27 02:41:54 -0600

    conexion de base de datos  a foms1 de usuario y contrase;a

--- Login.cs
+++ Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace proyecto_final_2._1
 {
@@ -14,14 +15,10 @@ namespace proyecto_final_2._1
     {
         public int contador = 0;
 
-      //  private object formRecuperar;
-      
-      // prueba 
         public Form1login()
         {
             InitializeComponent();
         }
-
 
         private void label2derecuperacion_Click(object sender, EventArgs e)
         {
@@ -29,18 +26,13 @@ namespace proyecto_final_2._1
             contrasena_olvidada contrasenaOlvidada = new contrasena_olvidada();
             contrasenaOlvidada.ShowDialog();
             this.Show();
-
         }
 
-
-        //creacion del ojo para ver contraseña
         private void pictureBox1_Click_2(object sender, EventArgs e)
         {
-            
             if (contador == 0)
             {
                 txtcontraseña.PasswordChar = '\0';
-
                 contador = 1;
             }
             else
@@ -50,20 +42,16 @@ namespace proyecto_final_2._1
             }
         }
 
-        // funcion para que cuando el usuario escriba sea de color negro 
         private void textBoxusuario_Enter(object sender, EventArgs e)
         {
-            
-
+            // Tu código personalizado si deseas
         }
 
-        // funcion  para que aparezca un mensaje en el textbox de color gris
         private void textBoxusuario_Leave(object sender, EventArgs e)
         {
-
+            // Tu código personalizado si deseas
         }
 
-        // evento para crear usuario
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -72,15 +60,49 @@ namespace proyecto_final_2._1
             this.Show();
         }
 
-
         private void btnlogin_Click(object sender, EventArgs e)
         {
+            string usuario = textBoxusuario.Text;
+            string contraseña = txtcontraseña.Text;
 
+            //  direccion del servidor completa de azure y agregar los datos del servidor, base de datos y password del servidor
+            string connectionString = ("Server = tcp:proyectoprogramacion.database.windows.net,1433; Initial Catalog = proyectofinal; Persist Security Info = False; User ID = proyectofinal;Password=2025*umg;MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conexion.Open();
+                    string consulta = "SELECT COUNT(*) FROM usuarios1 WHERE USUARIO = @usuario AND PASSWORD = @contraseña";
+
+                    using (SqlCommand comando = new SqlCommand(consulta, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@usuario", usuario);
+                        comando.Parameters.AddWithValue("@contraseña", contraseña);
+
+                        int resultado = (int)comando.ExecuteScalar();
+
+                        if (resultado > 0)
+                        {
+                            MessageBox.Show("Inicio de sesión exitoso.");
+                            // Aquí puedes abrir otro formulario, por ejemplo:
+                            // MenuPrincipal menu = new MenuPrincipal();
+                            // menu.Show();
+                            // this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuario o contraseña incorrectos.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar con la base de datos: " + ex.Message);
+                }
+            }
         }
-
-       
     }
 }
 
-
-// bloeqo
